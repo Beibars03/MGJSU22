e643c6ca8c6207c65f640a382e5245b777f02717
999MeshDi666 <havkpzz@gmail.com>
2022-11-07 20:55:01 +0600

    first commit

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,11 @@
+import "./loader.css";
+import { Container } from "react-bootstrap"
+
+const Loader = () =>{
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
+export default Loader
