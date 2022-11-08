815b672326917145b55c8fae151a6ec49d4e50d5
999MeshDi666 <havkpzz@gmail.com>
2022-11-08 14:47:32 +0600

    clouds animation

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,11 +1,41 @@
 import "./loader.css";
 import { Container } from "react-bootstrap"
+import cloud from "../../images/clouds/cloud.webp"
 
+const clouds = [
+    {
+        src: cloud,
+        class: "loader-cloud",
+        alt: "cloud1"
+    },
+    {
+        src: cloud,
+        class: "loader-cloud",
+        alt: "cloud2"
+    },
+    {
+        src: cloud,
+        class: "loader-cloud",
+        alt: "cloud3"
+    },
+
+]
 const Loader = () =>{
     return(
-        <Container>
-            
-        </Container>
+        <section className="loader">
+            <Container>
+                
+                <div className="clouds">
+                    {clouds.map((image, index)=>
+                            <img src={image.src} className={image.class} alt={image.alt} key={index} />
+                        )}
+                </div>
+          
+            </Container>
+                
+
+            </section>
+        
     )
 }
 export default Loader
