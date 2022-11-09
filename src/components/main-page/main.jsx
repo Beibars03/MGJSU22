d05529bc9c17d8f75e6251d310a41a6c21efc960
999MeshDi666 <havkpzz@gmail.com>
2022-11-09 22:52:08 +0600

    gif animation on main page

--- src/components/main-page/main.jsx
+++ src/components/main-page/main.jsx
@@ -1,11 +1,18 @@
 import "./main.css";
 import { Container } from "react-bootstrap";
+import venus from "../../images/greek/venus1.gif";
+import { Link } from "react-router-dom";
 
 const Main = () =>{
     return(
-        <section className="main">
+        <section className="main texture">
             <Container>
-                <h1> Main Page</h1>
+                <img src={venus} className="venus" alt="venus"/>
+                <div className="main_content">
+                    <p className="main_subtitle">Алматы. 7-9 Декабря</p>
+                    <h1 className="main_title">GameJam <br/> 2022</h1>
+                    <Link to="/" className="main_reg-btn">Регистрация</Link>
+                </div>
             </Container>
         </section>
         
