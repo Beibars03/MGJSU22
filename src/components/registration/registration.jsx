9d551bbfb54655180d600b820aafff0d1f8e8809
Vladimir Monomakh <red.freedomight@gmail.com>
2022-11-14 11:20:34 +0600

    add registration and text edit

--- src/components/registration/registration.jsx
+++ src/components/registration/registration.jsx
@@ -7,34 +7,47 @@ const formFields = [
     {
         type: "text",
         placeholder: "название команды",
+        name: "entry.622377088",
     },
     {
         type: "text",
         placeholder: "учебное заведение",
+        name: "entry.966331574",
     },
     {
         type: "email",
         placeholder: "электронная почта",
+        name: "entry.1842236906",
     },
     {
-        type: "tel",
+        type: "number",
         placeholder: "номер телефона",
+        name: "entry.1419582377",
     },
     {
         type: "text",
         placeholder: "капитан",
+        name: "entry.1601897725",
     },
     {
         type: "text",
         placeholder: "участник",
+        name:"entry.1688673403",
     },
     {
         type: "text",
         placeholder: "участник",
+        name: "entry.1712183872",
     },
     {
         type: "text",
         placeholder: "участник",
+        name: "entry.752129752",
+    },
+    {    
+        type: "text",
+        placeholder: "участник",
+        name:"entry.1555555961",
     }
 
 ]
@@ -47,12 +60,37 @@ const Registration = () =>{
                         <h2 className="general-title">Регистрация на SUMGJ22</h2>
                         <p className="general-subtitle">Регистрация команды</p>
                     </div>
-                    <form method="post"  className="reg-form">
+{/*                     
+                    <script type="text/javascript">var submitted=false;</script> */}
+					<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;" onload="if(submitted)  {window.location='thanks.html';}"></iframe>
+					{/* <form 
+						method="POST" 
+						action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxBG_we1ZXQ0yBwIs14s3CoAEgNkON1tx0ZpWmU20BjDmLkQ/formResponse?chromeless=true"
+						className="reg-form"
+						target="hidden_iframe" onsubmit="submitted=true;">
+						<input name="entry.622377088" type="text" className="reg-control" placeholder="название команды" required style=""/>
+						<input name="entry.1842236906" type="email" className="reg-control" aria-describedby="emailHelp" placeholder="электронная почта" required/>
+						<input name="entry.1419582377" type="number" className="reg-control" placeholder="номер телефона" required/>
+						<input name="entry.1601897725" type="text" className="reg-control" placeholder="капитан" required/>
+						<input name="entry.1688673403" type="text" className="reg-control" placeholder="участник" required/>
+						<input name="entry.1712183872" type="text" className="reg-control" placeholder="участник"/>
+					
+						<input name="entry.752129752" type="text" className="reg-control" placeholder="участник"/>
+						<input name="entry.1555555961" type="text" className="reg-control" placeholder="участник"/>
+						<input name="entry.966331574" type="text" className="reg-control" placeholder="учебное заведение" required/>
+                        <input type="submit" value="Регистрация" className="reg-btn"/>
+
+					<button type="submit" class="btn btn-outline-light button-submit">Регистрация</button>
+					</form> */}
+
+
+                    <form method="post"  className="reg-form" action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxBG_we1ZXQ0yBwIs14s3CoAEgNkON1tx0ZpWmU20BjDmLkQ/formResponse?chromeless=true">
                         {formFields.map((input, index)=>
                             <input 
                                 type={input.type} 
                                 placeholder={input.placeholder} 
                                 key={index} 
+                                name={input.name}
                                 className="reg-control"/>
                         )}
                         <input type="submit" value="Регистрация" className="reg-btn"/>
