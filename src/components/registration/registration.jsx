be8e5a9f16b7e1ee4c0ab9551f477d89b8614f7d
999MeshDi666 <havkpzz@gmail.com>
2022-11-14 15:49:43 +0600

    connect registration form with google form

--- src/components/registration/registration.jsx
+++ src/components/registration/registration.jsx
@@ -3,88 +3,132 @@ import axios from 'axios'
 import "./registration-styles/registration.css";
 import { Container } from "react-bootstrap";
 import kempir from "../../images/turkic/kempir.gif";
-
+import { GoogleFormProvider, useGoogleForm, useShortAnswerInput } from 'react-google-forms-hooks'
 
 const formFields = [
     {
-        name: "team",
+        // name: "team",
         type: "text",
         placeholder: "название команды",
-        // name: "entry.622377088",
+        name: "entry.622377088",
     },
     {
-        name: "study",
+        // name: "study",
         type: "text",
         placeholder: "учебное заведение",
-        // name: "entry.966331574",
+        name: "entry.966331574",
     },
     {
-        name: "email",
+        // name: "email",
         type: "email",
         placeholder: "электронная почта",
-        // name: "entry.1842236906",
+        name: "entry.1842236906",
     },
     {
-        name: "tel",
+        // name: "tel",
         type: "tel",
         placeholder: "номер телефона",
-        // name: "entry.1419582377",
+        name: "entry.1419582377",
     },
     {
-        name: "captain",
+        // name: "captain",
         type: "text",
         placeholder: "капитан",
-        // name: "entry.1601897725",
+        name: "entry.1601897725",
     },
     {
-        name: "participant1",
+        // name: "participant1",
         type: "text",
         placeholder: "участник",
-        // name:"entry.1688673403",
+        name: "entry.1688673403",
     },
     {
-       name: "participant2",
+    //    name: "participant2",
         type: "text",
         placeholder: "участник",
-        // name: "entry.1712183872",
+        name: "entry.1712183872",
     },
     {
-        name: "participant3",
+        // name: "participant3",
         type: "text",
         placeholder: "участник",
-        // name: "entry.752129752",
+        name: "entry.752129752",
     },
     {
-        name: "participant4",
+        // name: "participant4",
         type: "text",
         placeholder: "участник",
-        // name:"entry.1555555961",
+        name: "entry.1555555961",
     },
  
 
 ]
 
-
+// const formFields2 = [
+//     {
+//         type: "text",
+//         placeholder: "Просто вопрос",
+//         name: "entry.318924079",
+        
+//     },
+//     {
+//         type: "text",
+//         placeholder: "Просто вопрос2",
+//         name: "entry.69042969",
+        
+//     }
+// ]
 const Registration = () =>{
   
-    const [data, setData] = useState({})
-    const handleChange = (e) =>{
-       const {name, value} = e.target;
-       setData({
-            ...data,
-            [name]: value
-        })
-    }
+    // const [data, setData] = useState({})
+    // const handleChange = (e) =>{
+    //    const {name, value} = e.target;
+    //    setData({
+    //         ...data,
+    //         [name]: value
+    //     })
+    // }
   
     
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        axios.post('https://sheet.best/api/sheets/310bab3f-4e01-4136-aded-2559922f0194', data)
-        .then(
-                document.getElementById("reg-form").reset(),
-        )
+    // const handleSubmit = (e) =>{
+    //     e.preventDefault()
+    //     axios.post('https://sheet.best/api/sheets/310bab3f-4e01-4136-aded-2559922f0194', data)
+    //     .then(
+    //             document.getElementById("reg-form").reset(),
+    //     )
+    // }
+    const [formData, setFormData] = useState({
+        "entry.622377088": "",
+        "entry.966331574": "",
+        "entry.1842236906": "",
+        "entry.1419582377": "",
+        "entry.1601897725": "",
+        "entry.1688673403": "",
+        "entry.1712183872": "",
+        "entry.752129752": "",
+        "entry.1555555961": "",
+        
+    });
+    const handleInputData = (input) => (e) => {
+        const { value } = e.target;
+    
+        setFormData((prevState) => ({
+          ...prevState,
+          [input]: value
+        }));
+    };
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let url = `https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxBG_we1ZXQ0yBwIs14s3CoAEgNkON1tx0ZpWmU20BjDmLkQ/formResponse?entry.622377088=${formData["entry.622377088"]}&entry.966331574=${formData["entry.966331574"]}&entry.1842236906=${formData["entry.1842236906"]}&entry.1419582377=${formData["entry.1419582377"]}&entry.1601897725=${formData["entry.1601897725"]}&entry.1688673403=${formData["entry.1688673403"]}&entry.1712183872=${formData["entry.1712183872"]}&entry.752129752=${formData["entry.752129752"]}&entry.1555555961=${formData["entry.1555555961"]}`;
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        });
+       
     }
-    console.log(data)
     return(
         <section className="registration page">
             <Container className="wrapper">
@@ -93,6 +137,7 @@ const Registration = () =>{
                         <h2 className="general-title">Регистрация на SUMGJ22</h2>
                         <p className="general-subtitle">Регистрация команды</p>
                     </div>
+                 
                     {/* action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxBG_we1ZXQ0yBwIs14s3CoAEgNkON1tx0ZpWmU20BjDmLkQ/formResponse?chromeless=true" */}
                     <form method="post"  className="reg-form" id="reg-form" onSubmit={handleSubmit}>
                         {formFields.map((input, index)=>
@@ -101,8 +146,10 @@ const Registration = () =>{
                                 placeholder={input.placeholder} 
                                 key={index} 
                                 name={input.name}
+                                value={formData[input.name]}
                                 className="reg-control"
-                                onChange={handleChange}
+                                onChange={handleInputData(input.name)}
+                                required
                             />
                              
                         )}
