86757f29291247ab2fac75bf7546c2a96240d8d8
999MeshDi666 <havkpzz@gmail.com>
2022-11-14 14:04:35 +0600

    post in google sheets option

--- src/components/registration/registration.jsx
+++ src/components/registration/registration.jsx
@@ -7,49 +7,58 @@ import kempir from "../../images/turkic/kempir.gif";
 
 const formFields = [
     {
+        name: "team",
         type: "text",
         placeholder: "название команды",
-        name: "entry.622377088",
+        // name: "entry.622377088",
     },
     {
+        name: "study",
         type: "text",
         placeholder: "учебное заведение",
-        name: "entry.966331574",
+        // name: "entry.966331574",
     },
     {
+        name: "email",
         type: "email",
         placeholder: "электронная почта",
-        name: "entry.1842236906",
+        // name: "entry.1842236906",
     },
     {
+        name: "tel",
         type: "tel",
         placeholder: "номер телефона",
-        name: "entry.1419582377",
+        // name: "entry.1419582377",
     },
     {
+        name: "captain",
         type: "text",
         placeholder: "капитан",
-        name: "entry.1601897725",
+        // name: "entry.1601897725",
     },
     {
+        name: "participant1",
         type: "text",
         placeholder: "участник",
-        name:"entry.1555555961",
+        // name:"entry.1688673403",
     },
     {
+       name: "participant2",
         type: "text",
         placeholder: "участник",
-        name:"entry.1688673403",
+        // name: "entry.1712183872",
     },
     {
+        name: "participant3",
         type: "text",
         placeholder: "участник",
-        name: "entry.1712183872",
+        // name: "entry.752129752",
     },
     {
+        name: "participant4",
         type: "text",
         placeholder: "участник",
-        name: "entry.752129752",
+        // name:"entry.1555555961",
     },
  
 
@@ -73,8 +82,9 @@ const Registration = () =>{
         axios.post('https://sheet.best/api/sheets/310bab3f-4e01-4136-aded-2559922f0194', data)
         .then(
                 document.getElementById("reg-form").reset(),
-            )
+        )
     }
+    console.log(data)
     return(
         <section className="registration page">
             <Container className="wrapper">
@@ -83,13 +93,18 @@ const Registration = () =>{
                         <h2 className="general-title">Регистрация на SUMGJ22</h2>
                         <p className="general-subtitle">Регистрация команды</p>
                     </div>
-                    <form method="post"  className="reg-form">
+                    {/* action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxBG_we1ZXQ0yBwIs14s3CoAEgNkON1tx0ZpWmU20BjDmLkQ/formResponse?chromeless=true" */}
+                    <form method="post"  className="reg-form" id="reg-form" onSubmit={handleSubmit}>
                         {formFields.map((input, index)=>
                             <input 
                                 type={input.type} 
                                 placeholder={input.placeholder} 
                                 key={index} 
-                                className="reg-control"/>
+                                name={input.name}
+                                className="reg-control"
+                                onChange={handleChange}
+                            />
+                             
                         )}
                         <input type="submit" value="Регистрация" className="reg-btn"/>
                     </form>
